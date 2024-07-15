5a11b16ca96d35d9fc9507ecf319d42c9d3c32c7
choijh4161@gmail.com <21900764@handong.ac.kr>
2024-07-16 01:04:29 +0900

    feat: Add Setting

--- Assets/Scripts/HomeGM.cs
+++ Assets/Scripts/HomeGM.cs
@@ -5,39 +5,71 @@ using TMPro;
 
 public class HomeGM : MonoBehaviour
 {
-    public Button playButton;
-    public Button enhanceButton;
+    // 3 parts
+    public GameObject homeParts;
+    public GameObject settingParts;
+    public GameObject enhanceParts;
+
+    // core
+    public Button homeButton;
     public Button settingButton;
+    public Button enhanceButton;
     public Button exitButton;
+
+    // for Home Parts
+    public Button playButton;
     public Button nextStageButton;
     public Button beforeStageButton;
     public TMP_Text stageText;
-
     private int currentStage = 1;
+
+    // for Setting Parts
+    public Button playerSettingButton;
+    public Button towerSettingButton;
+    public GameObject playerSettingParts;
+    public GameObject towerSettingParts;
+    
 
     void Start()
     {
         playButton.onClick.AddListener(OnplayButtonClicked);
-        enhanceButton.onClick.AddListener(OnEnhanceButtonClicked);
+
+        homeButton.onClick.AddListener(OnHomeButtonClicked);
         settingButton.onClick.AddListener(OnSettingButtonClicked);
+        enhanceButton.onClick.AddListener(OnEnhanceButtonClicked);
+        
         nextStageButton.onClick.AddListener(OnNextStageButtonClicked);
         beforeStageButton.onClick.AddListener(OnBeforeStageButtonClicked);
+
+        playerSettingButton.onClick.AddListener(OnPlayerSettingButtonClicked);
+        towerSettingButton.onClick.AddListener(OnTowerSettingButtonClicked);
 
         UpdateStageText();
     }
 
-    void OnplayButtonClicked()
-    {
-        // 예시로 'GameScene'이라는 이름의 씬을 로드합니다.
-        Debug.Log("Play Button Clicked");
-        // SceneManager.LoadScene("SampleScene"); 
-        SceneManager.LoadScene("Stage" + currentStage.ToString()); 
-    }
 
+    // core
+    void OnHomeButtonClicked()
+    {
+        homeParts.SetActive(true);
+        settingParts.SetActive(false);
+        enhanceParts.SetActive(false);
+    }
+    void OnSettingButtonClicked()
+    {
+        // Setting 버튼 클릭 시 동작 정의
+        Debug.Log("Setting Button Clicked");
+        homeParts.SetActive(false);
+        settingParts.SetActive(true);
+        enhanceParts.SetActive(false);
+    }
     void OnEnhanceButtonClicked()
     {
         // Enhance 버튼 클릭 시 동작 정의
         Debug.Log("Enhance Button Clicked");
+        homeParts.SetActive(false);
+        settingParts.SetActive(false);
+        enhanceParts.SetActive(true);
     }
     void OnExitButtonClicked()
     {
@@ -45,12 +77,14 @@ public class HomeGM : MonoBehaviour
         // Application.Quit();
     }
 
-    void OnSettingButtonClicked()
+    // for Home parts
+    void OnplayButtonClicked()
     {
-        // Setting 버튼 클릭 시 동작 정의
-        Debug.Log("Setting Button Clicked");
+        // 예시로 'GameScene'이라는 이름의 씬을 로드합니다.
+        Debug.Log("Play Button Clicked");
+        // SceneManager.LoadScene("SampleScene"); 
+        SceneManager.LoadScene("Stage" + currentStage.ToString()); 
     }
-
     void OnNextStageButtonClicked()
     {
         currentStage++;
@@ -69,5 +103,19 @@ public class HomeGM : MonoBehaviour
     void UpdateStageText()
     {
         stageText.text = "Stage " + currentStage;
+    }
+
+     // for Setting parts
+    void OnPlayerSettingButtonClicked()
+    {
+        Debug.Log("Player Setting Button Clicked");
+        playerSettingParts.SetActive(true);
+        towerSettingParts.SetActive(false);    
+    }
+    void OnTowerSettingButtonClicked()
+    {
+        Debug.Log("Tower Setting Button Clicked");
+        playerSettingParts.SetActive(false);
+        towerSettingParts.SetActive(true);
     }
 }
